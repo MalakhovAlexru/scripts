92bbc607529e42b10da20a7ab061443a4109e28c
Александр Малахов <malakhovspb@gmail.com>
2018-08-14 18:03:53 +0300

    some changes

--- AL/Initial install/scripts/Client/Helpers/someFunc.sh
+++ AL/Initial install/scripts/Client/Helpers/someFunc.sh
@@ -11,3 +11,17 @@ else
 fi
 done
 fi
+
+
+# проверка ввода
+read -p "Please enter something: " userInput
+
+# Check if string is empty using -z. For more 'help test'    
+if [[ -z "$userInput" ]]; then
+   printf '%s\n' "No input entered"
+   exit 1
+else
+   # If userInput is not empty show what the user typed in and run ls -l
+   printf "You entered %s " "$userInput"
+   ls -l
+fi
